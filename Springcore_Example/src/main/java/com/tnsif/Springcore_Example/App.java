2b2876a7c4b27188d76e387a152574a34cc5265c
Aravind2005-tech <aravindmvit03@gmail.com>
2025-08-08 18:25:08 +0530

    pushed

--- Springcore_Example/src/main/java/com/tnsif/Springcore_Example/App.java
+++ Springcore_Example/src/main/java/com/tnsif/Springcore_Example/App.java
@@ -1,5 +1,6 @@
 package com.tnsif.Springcore_Example;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Hello world!
  *
@@ -9,5 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+   ApplicationContext var= new ClassPathXmlApplicationContext("applicatioincontext.xml");
+  
+   College c1=var.getBean("smvec",College.class);
+   
+   c1.display();
     }
 }
